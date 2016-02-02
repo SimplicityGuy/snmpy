861037185c5ba8ea630e0145d045593a6ab366ce
Robert Wlodarczyk <rwlodarc@impinj.com>
2016-02-02 08:07:11 -0800

    Using version from `__init__.py`

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
 from codecs import open
 from os import path
+from re import compile
+from ast import literal_eval
 
 here = path.abspath(path.dirname(__file__))
+_version_re = compile(r'__version__\s+=\s+(.*)')
+
+with open('snmpy/__init__.py', 'rb') as f:
+    version = str(literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='snmpy',
-    version='0.1.0',
+    version=version,
     description='Simple pysnmp wrapper.',
     long_description=long_description,
     url='https://github.com/SimplicityGuy/snmpy',
